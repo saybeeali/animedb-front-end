a246b7b2ab6e30baba6a1d8ff63475e25d8a6809
ekelly55 <eamonn.michael.kellyr@email.com>
2022-09-18 15:18:07 -0400

    show page renders dummy data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,17 @@
-function Home() {
+import AnimeCard from "../components/AnimeCard";
+import anime from "../sample_data";
+
+function Home(props) {
     return (
       <div className="Home">
         <h1>This will be a list of anime</h1>
+        <AnimeCard anime={anime}/>
+        <AnimeCard anime={anime}/>
+        <AnimeCard anime={anime}/>
+        <AnimeCard anime={anime}/>
+        <AnimeCard anime={anime}/>
+        <AnimeCard anime={anime}/>
+        <AnimeCard anime={anime}/>
       </div>
     );
   }
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,7 +1,13 @@
+import anime from "../sample_data";
+
 function Show() {
     return (
       <div className="Show">
-        <h1>Here is an anime</h1>
+        <h1>{anime[0].title}</h1>
+        <img src={anime[0].image}></img>
+        <p>medium: {anime[0].medium}</p>
+        <p>air dates: {anime[0].air_dates}</p>
+        <a href={anime[0].traiiler} target="_blank" rel="noreferrer">Trailer</a>
       </div>
     );
   }
