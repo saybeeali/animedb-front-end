8a8f123218dedbdb82021c15fc7284afcf90e208
7vma <robles23david@gmail.com>
2022-09-19 08:05:26 -0700

    added branch, logo and container

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import Carousel from './components/Carousel';
 import Home from './pages/Home';
 import Show from './pages/Show';
+import Nav from './components/Nav';
 import './App.css';
 import {Routes, Route, Outlet} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Sidebar/>
-      <h1>Future home of AnimeDB</h1>
-      <Outlet/>
-      <Routes>
-        <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/:id" element={<Show/>}/>
-      </Routes>
+      <div className='wrapper'>
+        <header className='header'> <Header/> </header>
+        <aside className='sidebar'> <Sidebar/> </aside>
+        <div className='content'> <h1>Content</h1> </div>
+        <footer className='footer'> <h1>footer</h1> </footer>
+        {/* <h1>Future home of AnimeDB</h1>
+        <Outlet/>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/:id" element={<Show/>}/>
+        </Routes> */}
+      </div>
     </div>
   );
 }
