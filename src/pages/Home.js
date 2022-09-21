8c2685b3b680281eea5fe5a588dfe623d784139a
Sohaib Ali <saybe.ali0618@gmail.com>
2022-09-21 11:43:01 -0400

    added images to home page'

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,26 @@
 import AnimeCard from "../components/AnimeCard";
 
-import animeList from "../sample_data";
+
 import {useState, useEffect} from "react"
 
 
 function Home(props) {
 
-    const [newAnimeList, setNewAnimeList] = useState(null)
+    const [newAnimeList, setNewAnimeList] = useState([])
 
-    const animeURL = ""
-    //once we have the backend deployed we'll fill in the url here 
-    //and use it to fetch the data from the backend
-    const getAnime = async () => {
-        try {
-            const response = await fetch(animeURL)
-            //console.log(response)
-            const allAnime = await response.json()
-            setNewAnimeList(allAnime)
-        } catch (err) {
-            //console.log(err)
-        }
-    }
+    const animeURL = "https://animedb-back-end.herokuapp.com/anime"
+  
 
-useEffect(() => {getAnime()}, [])
+useEffect(() => {
+    fetch(animeURL)
+    .then((res) => res.json())
+    .then((json) => {
+        console.log(json)
+        setNewAnimeList(json)
+    })
+    .catch(console.error)
+}, [])
+    
 
 
     const loading = () => {
@@ -32,7 +30,7 @@ useEffect(() => {getAnime()}, [])
     }
 
     const loaded = (props) => {
-        return animeList.map((anime, idx) => {
+        return newAnimeList.map((anime, idx) => {
             return (
                 <div className="animeIndex">
                     <AnimeCard anime={anime}/>
@@ -46,12 +44,37 @@ useEffect(() => {getAnime()}, [])
     return (
       <div className="Home">
       <div className="row row-cols-1 row-cols-md-4 g-4">
-        {animeList && animeList.length ? loaded() : loading()}
+        <section className="container">
+            {newAnimeList.map((anime)=>{
+                return (
+                    <div>
+                        <h1>{anime.title}</h1>
+                    <img src= {anime.images.jpg.image_url}/>
+                    </div>
+                    
+                )
+            }
+            )}
+        </section>
+   
+        {newAnimeList && newAnimeList.length ? loaded() : loading()}
       </div>
       </div>
     );
 }
   
   export default Home;
+    //once we have the backend deployed we'll fill in the url here 
+    //and use it to fetch the data from the backend
+    // const getAnime = async () => {
+    //     try {
+    //         const response = await fetch(animeURL)
+    //         //console.log(response)
+    //         const allAnime = await response.json()
+    //         setNewAnimeList(allAnime)
+    //     } catch (err) {
+    //         //console.log(err)
+    //     }
+    // }
 
   
