9ac415a74906e09c0fe78f344f6f2a9fa3d8136d
Sohaib Ali <saybe.ali0618@gmail.com>
2022-09-22 20:11:50 -0400

    review sent to backend!!!!!!!!!

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -2,19 +2,45 @@ import { toHaveDescription } from '@testing-library/jest-dom/dist/matchers';
 import React, { useState } from 'react';
 import ReviewForm from './ReviewForm';
 import ReviewList from './ReviewList';
+import { useParams } from "react-router-dom";
 
-function Reviews() {
+function Reviews(props) {
 
     const [reviews, setReviews] = useState([])
 
     function addReview(data) {
         setReviews([reviews,data])
     }
+    const  id = useParams()
+    const animeUrl = `https://animedb-back-end.herokuapp.com/anime/${id.id}`
+    console.log(id.id)
+
+
+    const createReview = async (reviewData) => {
+        console.log(reviewData)
+        try {
+        const newReview = await fetch (animeUrl, {
+            method: "POST", 
+            mode: 'cors',
+            headers : {
+                "Content-Type": "application/json", 
+                'Access-Control-Allow-Origin' : "*"
+            },
+            body: JSON.stringify({
+                post:reviewData
+            }),
+        });
+        console.log(await newReview.json)
+        props.getAnime()
+    } catch (err) {
+        console.log(err)
+    }
+};
 
     return (
         <div className='Reviews'>
             <h1>Reviews</h1>
-                <ReviewForm addReview={addReview}/>
+                <ReviewForm createReview={createReview}/>
             
             
             <main><ReviewList reviews={reviews} /></main>
